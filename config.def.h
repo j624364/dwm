e55e0161c209d0ca6cb2062a74139f92acd3add8
j624364 <49117678+j624364@users.noreply.github.com>
2023-04-08 20:57:48 +0100

    Add emacs keybind

--- config.def.h
+++ config.def.h
@@ -78,11 +78,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-config", "~/.config/rofi/rofimenu.rasi" };
 static const char *termcmd[]  = { "kitty", NULL };
+static const char *emacsclientcmd[]  = { "emacsclient", "-c", "-a", "'emacs'", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = emacsclientcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
