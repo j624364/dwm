33c92bbc781dbc49eec75602cb427b612db39964
j624364 <49117678+j624364@users.noreply.github.com>
2023-04-08 19:23:06 +0100

    Add names for tags

--- config.def.h
+++ config.def.h
@@ -27,8 +27,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = {
-	"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-	"11", "12", "13", "14", "15", "16", "17", "18", "19", "Desktop"
+	"chromium", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+	"firefox", "12", "13", "14", "15", "16", "17", "18", "spotify", "desktop"
 };
 
 static const Rule rules[] = {
