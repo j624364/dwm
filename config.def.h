01ae5dcbfc1c18b8b791171ec20ccf0067501a3d
j624364 <49117678+j624364@users.noreply.github.com>
2024-11-11 20:59:37 +0000

    Fix language switch script not being called

--- config.def.h
+++ config.def.h
@@ -79,7 +79,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-modi", "drun", "-show", "drun", "-config", "~/.config/rofi/rofimenu.rasi" };
-static const char *languagecmd[] = { "~/Scripts/SwitchKeyboardLayout.py" };
+static const char *languagecmd[] = { "/usr/bin/python3", "/home/joe/Scripts/SwitchKeyboardLayout.py" };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *emacsclientcmd[]  = { "emacsclient", "-c", "-a", "'emacs'", NULL };
 
